ca06651b9742752512a2118501f3165455e9c3ad
Hatem Elsisi <elsisi@uni.minerva.edu>
2024-10-03 17:07:26 -0300

    Initial commit

--- fazkerbot.py
+++ fazkerbot.py
@@ -7,8 +7,9 @@ import requests
 from pytz import timezone
 
 # Set up bot and chat details
-TOKEN = "7365741617:AAE_TGjBcXAt81r5Hfe4rFlci-Os0q50nPk"  # Use the actual bot token
-CHAT_ID = "-1002450375757"  # Use the actual chat ID for your private channel
+import os
+TOKEN = os.getenv("TELEGRAM_TOKEN")  # Replace with environment variable
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")  # Replace with environment variable
 bot = telegram.Bot(token=TOKEN)
 
 # Folder paths for images
